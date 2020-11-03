fbc1f7d45fc692bd113be90d55c8774fc3a443af
sserges <agounkeserge@gmail.com>
2020-11-03 18:30:34 +0000

    Bringing Redux State Into HomeScreen - useDispatch & useSelector

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,4 @@
-import axios from axios;
+import axios from 'axios';
 
 import {
   PRODUCT_LIST_REQUEST,
@@ -10,16 +10,19 @@ export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const {data} =  await axios.get('/api/products');
+    const { data } = await axios.get('/api/products');
 
     dispatch({
-        type: PRODUCT_LIST_SUCCESS,
-        payload: data
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
     });
   } catch (error) {
-      dispatch({
-          type: PRODUCT_LIST_FAIL,
-          payload: error.response && error.response.data.message ? error.response.data.message : error.message
-      });
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
