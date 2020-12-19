95ef90a9d7f35de4c17eb8753a5821314ab6854e
sserges <agounkeserge@gmail.com>
2020-12-19 11:33:15 +0000

    input validation with joi added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,28 @@
-import asyncHandler from 'express-async-handler';
+import asyncHandler from 'express-async-handler'
+import joi from 'joi'
 
-import generateToken from '../utils/generateToken.js';
-import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js'
+import User from '../models/userModel.js'
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 export const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const authUserDataSchema = joi.object({
+    email: joi.string().email().trim().required(),
+    password: joi.string().trim().required(),
+  })
 
-  const user = await User.findOne({ email });
+  const result = authUserDataSchema.validate(req.body)
+
+  if (result.error) {
+    res.status(400)
+    throw new Error(result.error.details[0].message)
+  }
+
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
     res.json({
@@ -18,31 +31,44 @@ export const authUser = asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(401);
-    throw new Error('Invalid email or password');
+    res.status(401)
+    throw new Error('Invalid email or password')
   }
-});
+})
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
 export const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const registerUserDataSchema = joi.object({
+    name: joi.string().trim().required(),
+    email: joi.string().email().trim().required(),
+    password: joi.string().trim().required(),
+  })
 
-  const userExists = await User.findOne({ email });
+  const result = registerUserDataSchema.validate(req.body)
+
+  if (result.error) {
+    res.status(400)
+    throw new Error(result.error.details[0].message)
+  }
+
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
 
   if (userExists) {
-    res.status(400);
-    throw new Error('User already exists');
+    res.status(400)
+    throw new Error('User already exists')
   }
 
   const user = await User.create({
     name,
     email,
     password,
-  });
+  })
 
   if (user) {
     res.status(201).json({
@@ -51,18 +77,18 @@ export const registerUser = asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(400);
-    throw new Error('Invalid user data');
+    res.status(400)
+    throw new Error('Invalid user data')
   }
-});
+})
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 export const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id)
 
   if (user) {
     res.json({
@@ -70,9 +96,28 @@ export const getUserProfile = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-    });
+    })
   } else {
-    res.status(404);
-    throw new Error('User not found');
+    res.status(404)
+    throw new Error('User not found')
   }
-});
+})
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
